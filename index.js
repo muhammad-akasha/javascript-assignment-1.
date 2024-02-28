32d0a3eca7ebf8da79ce4552d80d02940990889e
Muhammad Akasha <153507162+muhammad-akasha@users.noreply.github.com>
2024-02-29 01:11:10 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//assigment -1-
+alert(" error! Please enter a valid password ");
+
+alert(" Welcome to Js land.. " + "\n" + " Happy Coding "); // (\n) mean to break a line.
+
+alert(
+  " welcome to js land" +
+    " \n " +
+    " prevent this page from creating additionals dialogs. "
+);
